58725412e980ec35315942e8269adc4df201951c
Lusca-rcg <lusca.rcg@gmail.com>
2024-01-06 16:48:31 -0300

    feat: criando header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import { HeaderConteiner } from "./styles"
+
+function Header() {
+    return (
+    <HeaderConteiner>
+        <h1>Todo List</h1>
+    </HeaderConteiner>
+    )
+}
+export default Header
---
+++ src/components/Header/styles.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const HeaderConteiner = styled.div`
+    display:flex;
+    justify-content: center;
+    background-color: #6d06a4;
+    color: white;
+    padding: 1.5%;
+    
+    `
