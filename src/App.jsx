547f21577c1c38b314a357585d4388865f0284a0
Lusca-rcg <lusca.rcg@gmail.com>
2024-01-06 16:30:14 -0300

    feat: criando o 'excluir tasks'

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,45 @@ import { GlobalStyle } from "./styles/Global/global"
 import { HeaderContainer } from "./components/Header/style"
 
 function App() {
-  const AddTask = (descricao) => {
+  const AddTask = (taskName) => {
     const newTask = [...tasks,
       {
         "id":Math.floor(Math.random() *10000),
-        "descricao": descricao,
+        "task": taskName,
         "status": pendente
       }
     ]
     setTask(newTask)
   }
   
-  const editTask = (id, newDescricao) => {
+  const editTask = (id, newTaskName) => {
     const newTask = todos.map((task) => 
-    task.id === id ? {...task, descricao: newDescricao} : task
+    task.id === id ? {...task, taskName: newTaskName} : task
     ) ;
     setTask(newTask) 
   }
 
-
-
-
+  const removeTask = (id) => {
+    const newTask = [...task]
+    const filter = newTask.filter((task) => task.id !== id)
+    setTask(filter)
+  }
 
   const [tasks,setTask] = useState([
     {
       id:1,
-      taskName:'estudar react'
+      taskName:'estudar react',
+      status: 'concluido'
     },
     {
       id:2,
-      taskName:'malhar'
+      taskName:'malhar',
+      status: 'em andamento'
     },
     {
       id:3,
-      taskName:'dormir'
+      taskName:'dormir',
+      status: 'pendente'
     }
   ])
 
